9d1f47e0fc5d725d2846cc235557e2290374d463
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-05-19 08:48:05 +0530

    helloworld.java
    Hello enter in java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class main{
+    public static void main(String args[]) //boilerplate code(hamesha write)
+    {
+        System.out.println("Hello github");// ln and \n used to space.
+    }
+}
