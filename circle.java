e245aa656b09c77ea0fdfe30e06ad66e429c1d75
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-05-20 19:15:23 +0530

    Add files via upload
    Area of the circle

---
+++ circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class circle {
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+
+        
+        float area=3.14*r*r;
+        System.out.println("area of the circle : " +area);
+    
+    }
+    
+}
