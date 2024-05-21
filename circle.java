79fc7fbf3b6776b277c23f7cb1266cc78d86abdb
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-05-21 19:30:04 +0530

    circle.java

--- circle.java
+++ circle.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class circle {
+public class Main {
     public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
 
-        
-        float area=3.14*r*r;
+        float rad=sc.nextFloat();
+        float area=3.14f*rad*rad;
         System.out.println("area of the circle : " +area);
     
     }
