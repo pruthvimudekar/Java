82193bc65d09eb2fe92a37256aa25fede736f048
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-05-23 19:07:35 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,14 @@
+public class largest {
+    public static void main(String args[]){
+    int a=50;
+    int b=30;
+    if(a>=b){
+        System.out.println("The no is greater than B :");
+
+    }
+    else{
+        System.out.println("The no is smaller than A: ");
+    }
+}
+    
+}
