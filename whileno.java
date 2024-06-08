a3dd7a371df2cadcfbe637999f5264a6277cd075
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-08 17:18:30 +0530

    Add files via upload

---
+++ whileno.java
@@ -0,0 +1,29 @@
+public class whileno {
+    public static void main(String[] args) {
+        
+        int counter = 1;
+        while (counter <=100){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+    
+}
+
+
+/*next line madhe lihanya sathi
+class HelloWorld {
+    public static void main(String[] args) {
+        
+     int counter = 1;
+     while (counter<=10){
+        System.out.print(counter + " ");
+        counter++;
+     }
+     System.out.println();
+    }
+}
+ * 
+ * output = 1 2 3 4 5 6 7 8 9 10 
+ * 
+ */
