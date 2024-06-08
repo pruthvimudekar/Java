a55c7827bf55bd58ca3fac1602039734e4f4f945
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-08 17:19:06 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,12 @@
+//time print karne ke liye 
+public class whileloop {
+    public static void main(String args[]){
+        int counter= 0;
+        while(counter<100){               // counter less than 100 times
+            System.out.println("Hello Pruthvi"); 
+            counter++;
+        }
+        System.out.println(" End of hello 100x");
+    }
+    
+}
