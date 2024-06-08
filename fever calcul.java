f04afd7a7f509939386dbc4169717e0420e470ad
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-08 11:37:01 +0530

    fever calcul.java

---
+++ fever calcul.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("enter the number :");
+         double temp=sc.nextDouble();
+         
+         
+         if (temp>100){
+            System.out.println("you have fever");
+            
+         }
+         else{
+             System.out.println("you dont have fever :");
+         }
+        
+        
+      
+    }
+}
