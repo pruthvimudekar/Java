18a72fca6031b76d81e89ba13ff1f05b91d76cc7
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-08 09:26:04 +0530

    Update oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+         
+         System.out.println("Enter the value : ");
+       int number= sc.nextInt();
+       
+       if(number% 2== 0){
+           System.out.println("EVEN IS FORMED :");
+           
+       }
+       else{
+           
+           System.out.println("odd");
+       }
+    }
+}
