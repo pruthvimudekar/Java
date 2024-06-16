ae3bfd9ade903d9f063a311cf2843342b46a51f5
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-16 22:39:49 +0530

    Add files via upload

---
+++ starforloop.java
@@ -0,0 +1,9 @@
+public class starforloop {
+    public static void main(String args[]){
+
+        for(int lines=1; lines<=4; lines ++ ){
+    System.out.println("* * * * ");
+        }
+    }
+    
+}
