c136b694422dbb1ada854ed9f18d7c5987941814
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-16 22:58:37 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void main(String args[]){
+
+        int counter =1;
+        do{
+            System.out.println("Hello Pruthvi ");
+            counter ++;
+        }
+        while(counter<=10);
+
+
+    }
+}
