61fd75e7824adaf20d119f71b83beec0d536308c
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-06-22 22:09:51 +0530

    Add files via upload

---
+++ ultastar.java
@@ -0,0 +1,13 @@
+/* ulta star print karnaysathi .... */
+
+public class ultastar {
+    public static void main(String args[]){
+       int n=4;
+        for(int line= 1; line<=n; line++){
+            for(int star=1; star<=n-line+1; star++){
+                System.out.print("*");
+            }
+                System.out.println();
+        }
+    }
+}
