8de1c4e7595a3ef6168163c81d81401b8c5b5162
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-07-10 19:37:45 +0530

    Add files via upload

---
+++ dividefun.java
@@ -0,0 +1,14 @@
+public class dividefun {
+    public static int caluc(int a, int b, int c){
+        int pro= a*b+c;
+        return pro;
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=8;
+        int c=9;
+        int pro= caluc(a,b,c);
+        System.out.println("Total value of : " +pro);
+    }
+    
+}
