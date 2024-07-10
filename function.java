69d87d616a1eb5a9289f10a3678210065e4ae9d5
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-07-10 19:36:07 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,10 @@
+public class function {
+    public static void helloworldpar(){  // fuction zale void he returntype ahe.
+ 
+        System.out.println("Hello pruthvi : ");
+        return ;
+    }
+    public static void main(String[] args) {  //main he main fuction ahe. public static he access specifiers. and 
+        helloworldpar();
+    }
+}
